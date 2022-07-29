f4d43a7499c64b34c7fea62afaee9462c211580b
FranklinSousaa <franklin.wilian.cc@gmail.com>
2022-07-29 14:28:24 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro();
+
+        System.out.println(cachorro);
+
+           /* int a = 25;
+            int b = 4;
+
+        System.out.println(" O Franklin tem: " + (a+b) + " anos !");*/
+    }
+
+}
